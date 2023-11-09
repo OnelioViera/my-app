005a490353e55019b965a631d7b82d2335ebdc4a
Onelio Viera <ojvwebdesign@gmail.com>
2023-11-09 17:53:13 -0500

    update files

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+      
       <h1 className="text-5xl">Test Next.js</h1>
       <p className='items-center'>Learn to code with Next.js</p>
-      </div>
+      
     </main>
   )
 }
